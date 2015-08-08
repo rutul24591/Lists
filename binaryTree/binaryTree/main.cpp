1d8d26aa5a90e871ef89529c509c93a801973c75
rutul <rutul591@gmail.com>
2015-08-07 17:11:31 -0700

    postorderprint

--- binaryTree/binaryTree/main.cpp
+++ binaryTree/binaryTree/main.cpp
@@ -13,7 +13,6 @@
 
 using namespace std;
 
-
 struct node{
     
     int data;
@@ -21,68 +20,94 @@ struct node{
     node* right;
 };
 
-node* getNewNode(int key){
+node* insert(node* root, int data);
+node* getNewNode(int data);
+bool search(node* root,int data);
+void preOrderPrint(node* root);
+void postOrderPrint(node* root);
 
-    node *n= new node();
-    n-> data = key;
+node* getNewNode(int data){
+
+    node* n= new node();
+    n-> data = data;
     n-> left = NULL;
     n-> right = NULL;
     return n;
 
 }
 
-node *insert(node *root, int key);
-node *getNewNode(int key);
-node *search(node *root,int key);
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    node *root= NULL;
-    insert(root,5);
-    insert(root,10);
-    insert(root,15);
-    insert(root,25);
-    insert(root,24);
-    insert(root,37);
-    int num;
-    cout<< "Enter the value of the number that is to be searched in the tree: "<< endl;
-    cin>> num;
-    if(search(root,num) == true){
-        cout<< " Looking for num in the tree... \n";
-        cout<< " number found in the tree at: "<< &num <<endl;
-    }
-    
-    return 0;
-}
 
-node* insert(node *root, int key){
+node* insert(node* root, int data){
+
     if(root == NULL){
-        root= getNewNode(key);
-        return root;
-    }else if(key < root -> data){
-        root -> left = insert(root->left, key);
+         root= getNewNode(data);
+    }else if(data <= root->data){
+        root->left = insert(root->left, data);
     }else{
-        root -> right = insert(root->right,key);
+        root->right = insert(root->right,data);
     }
     return root;
 }
 
 
-bool search(node *root, int key){
+bool search(node* root, int data){
 
     if( root == NULL){
         return false;
-    }else if(root -> data == key){
+    }else if(root->data == data){
         return true;
-    }else if( key <= root -> data){
-        return search(root ->left, key);
+    }else if( data <= root->data){
+        return search(root->left, data);
     }else{
-        return search(root ->right, key);
+        return search(root->right, data);
     }
 
 }
 
 
+void preOrderPrint(node* root){
+    if( root != NULL){
+        cout << root -> data << "\n\n"<< endl;
+         preOrderPrint( root->left );
+         preOrderPrint( root->right );
+    }
+
+}
+
+void postOrderPrint(node* root){
+    if(root != NULL){
+        postOrderPrint(root->left);
+        postOrderPrint(root->right);
+        cout<< root->data <<"\n\n" <<endl;
+    }
+
+}
+int main(int argc, char **argv) {
+    // insert code here...
+    node* root= NULL;
+    root = insert(root,5);
+    root = insert(root,10);
+    root = insert(root,15);
+    root = insert(root,25);
+    root = insert(root,24);
+    root = insert(root,37);
+    preOrderPrint(root);
+    postOrderPrint(root);
+   
+    int num;
+    cout << "Enter the value of the number that is to be searched in the tree: "<< endl;
+    cin >> num ;
+    bool result = search(root,num);
+    if(  result == true){
+        cout<< " Looking for num in the tree... \n";
+        cout<< " number found"<<endl;
+    }else{
+        cout<< "Number not found in the tree."<<endl;
+    }
+    
+   return 0;
+}
 
 
 
