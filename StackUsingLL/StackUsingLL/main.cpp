bc78e330dda2bd7ab867b3e1d047393c48dab12a
rutul <rutul591@gmail.com>
2015-08-12 20:26:06 -0700

    completedthepushnpopofstackusingLL

--- StackUsingLL/StackUsingLL/main.cpp
+++ StackUsingLL/StackUsingLL/main.cpp
@@ -13,27 +13,75 @@
 
 using namespace std;
 
-class node{
-public:
-    int data;
-    node* next;
-};
 
 class stackLL{
-    private:
+private:
+    struct node{
+        int data;
+        node* next;
+    };
+    
     node* top;
     int count;
     int capacity;
     
-    public:
+
+    
+public:
         stackLL(int max){
             top = NULL;
             capacity = max;
             count = 0;
         }
     
+    void push(int data){
+        if(count == capacity){
+            cout<< " The Stack is full..."<<endl;
+        }else{
+        
+            node *n = new node();
+            if(top == NULL){
+                n->data =data;
+                n->next =NULL;
+                top=n;
+                count++;
+            }else{
+                n->data= data;
+                n->next= top;
+                top=n;
+                count++;
+            }
+        }
+    }
     
     
-    
-    
+    void pop(){
+        if(top == NULL){
+            cout<< "There is no element to pop"<<endl;
+            }else{
+                node* out= top;
+                top= top->next;
+                count--;
+                cout<< top->data << " is the new top after popping"<<endl;
+                
+                
+        }
+    }
 };
+
+int main( int argc, char** argv){
+
+    stackLL *s1 = new stackLL(6);
+    
+    s1->push(1);
+    s1->push(2);
+    s1->push(3);
+    s1->push(4);
+    s1->push(5);
+    s1->push(6);
+    
+    s1->pop();
+    s1->pop();
+
+    return 0;
+}
