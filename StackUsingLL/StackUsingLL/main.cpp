7040aa6db1f5cc0a79dd1ca12b0ebb7290eab76b
rutul <rutul591@gmail.com>
2015-08-12 21:23:17 -0700

    pushpopprintimplemented

--- StackUsingLL/StackUsingLL/main.cpp
+++ StackUsingLL/StackUsingLL/main.cpp
@@ -38,7 +38,6 @@ public:
         if(count == capacity){
             cout<< " The Stack is full..."<<endl;
         }else{
-        
             node *n = new node();
             if(top == NULL){
                 n->data =data;
@@ -63,25 +62,41 @@ public:
                 top= top->next;
                 count--;
                 cout<< top->data << " is the new top after popping"<<endl;
-                
+                delete(out);
                 
         }
     }
+
+
+void print(){
+    node* temp= top;
+    while( temp != NULL){
+        cout<< temp->data<<endl;
+        temp= temp->next;
+        }
+    }
 };
-
-int main( int argc, char** argv){
-
-    stackLL *s1 = new stackLL(6);
     
-    s1->push(1);
-    s1->push(2);
-    s1->push(3);
-    s1->push(4);
-    s1->push(5);
-    s1->push(6);
-    
-    s1->pop();
-    s1->pop();
+int main(int argc, char** argv){
 
+    stackLL *s2 = new stackLL(100);
+    
+    cout<< "hello "<<endl;
+   
+    s2->push(1);
+    s2->push(2);
+    s2->push(3);
+    s2->push(4);
+    s2->push(5);
+    s2->push(6);
+    s2->push(7);
+    s2->push(8);
+    cout<<"The elements of stack are: "<<endl;
+    s2->print();
+    s2->pop();
+    s2->pop();
+    cout<< "The new elements of stack are: "<<endl;
+    s2->print();
+    delete s2;
     return 0;
 }
